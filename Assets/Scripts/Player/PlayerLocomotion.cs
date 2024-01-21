9e6fca11ce50ecfa4e00566e1c10a359195bd988
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-01-21 11:33:38 +0100

    Making Player move and also rotate where is player going

---
+++ Assets/Scripts/Player/PlayerLocomotion.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    private InputManager inputManager;
+    private Vector3 moveDirection;
+    
+    private Transform cameraObject;
+    private Rigidbody playerRigidbody;
+
+    public float movementSpeed = 7.0f;
+    public float rotationSpeed = 15.0f;
+    
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+    }
+    
+    public void HandleAllMovement()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+    
+    private void HandleMovement()
+    {
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        
+        moveDirection = moveDirection * movementSpeed;
+
+        Vector3 movementVelocity = moveDirection;
+        playerRigidbody.velocity = movementVelocity;
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        
+        if (targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+        
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        
+        transform.rotation = playerRotation;
+    }
+}
